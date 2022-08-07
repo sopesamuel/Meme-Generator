5d3c07204f8114986c5114ece18e40ca5de33fd1
sopesamuel <fasusimosope@gmail.com>
2022-08-07 11:25:17 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from './Components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+   <Navbar />
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Trollface from "/Users/mac/Documents/Meme Generator/my-app/src/Images/Troll Face.png"
+
+export default function Navbar (){
+    return (
+        <div className="navbar">
+            <span className="navbar-txt"> <img src={Trollface}></img>
+        <h1>Meme Generator</h1> </span> 
+        <h2>React Course - Project 3</h2>
+        </div>
+       
+    )
+}
